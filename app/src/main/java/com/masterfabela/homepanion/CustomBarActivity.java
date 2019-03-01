258b3966402629db101a03000cb1b08fe1dde3f1
masterfabela <fabelaretorcida@gmail.com>
2019-03-01 14:24:28 +0100

    Añadido codigo para barras superiores custom

--- app/src/main/java/com/masterfabela/homepanion/CustomBarActivity.java
+++ app/src/main/java/com/masterfabela/homepanion/CustomBarActivity.java
@@ -1,28 +1,22 @@
 package com.masterfabela.homepanion;
 
 import android.app.ActionBar;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.Toolbar;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Sample[] mSamples;
-    private GridView mGridView;
+public class CustomBarActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         putCustomActionBar(getCustomBar(R.layout.custom_bar));
     }
+
 
     public void putCustomActionBar(View customBar) {
         ActionBar actualActionBar = getActionBar();
@@ -38,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public View getCustomBar(int customBarID) {
+        LayoutInflater inflater = (LayoutInflater) Objects.requireNonNull(getActionBar())
+                .getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
         View customBarView = inflater.inflate(getResources().getLayout(customBarID), null);
         customBarView.findViewById(R.id.actionbar_done).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
         });
         return customBarView;
     }
+
 }
---
+++ app/src/main/java/com/masterfabela/homepanion/Sample.java
@@ -0,0 +1,13 @@
+package com.masterfabela.homepanion;
+
+import android.content.Intent;
+
+public class Sample {
+    int titleResId;
+    int descriptionResId;
+    Intent intent;
+    
+    public Sample(int titleResId){
+        // TODO: 01/03/2019 Tengo que acabar el recurso este googlesamples/android-DoneBar
+    }
+}
